eeac89f0e17a174ea2d2d018ed153267d439a813
Abel Simões <abelrmsimoes@gmail.com>
2021-08-31 10:44:32 -0300

    Configure database connection

--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ const app = express();
 const router = require("./router/router");
 const port = process.env.REST_API_PORT;
 
+const db = require("./database/config");
+
+db.then(() => {
+  console.log("Conexão realizada com sucesso");
+}).catch((e) => {
+  console.log("Ocorreu um erro ao Conectar-se");
+});
+
 app.use(express.json());
 app.use("/", (req, res) => {
   res.send({ success: "Primeira Rota Criada!" });
